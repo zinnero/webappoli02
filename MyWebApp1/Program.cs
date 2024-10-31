7094209bf057c4048e4c3e252a31d400b33364ef
oliver.zinner <oliver.zinner@devterest.com>
2024-10-31 10:23:02 +0100

    Bruno

--- MyWebApp1/Program.cs
+++ MyWebApp1/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! Greetings!");
+app.MapGet("/", () => "Hello World! Greetings Bruno!");
 
 app.Run();
